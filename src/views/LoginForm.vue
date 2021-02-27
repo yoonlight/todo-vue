7b95df8edcac25263683b724104f85ec883174fc
kjw <yoonlight@gmail.com>
2021-02-27 11:21:32 +0900

    design login form

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -1,47 +1,60 @@
 <template>
-  <v-form ref="form" v-model="valid" lazy-validation>
-    <v-text-field
-      v-model="name"
-      :counter="10"
-      :rules="nameRules"
-      label="Name"
-      required
-    ></v-text-field>
+  <v-container>
+    <v-responsive height="85vh" class="d-flex align-center text-center pa-2">
+      <v-card>
+        <v-card-text>
+          <v-form ref="form" v-model="valid" lazy-validation>
+            <v-text-field
+              v-model="name"
+              :counter="10"
+              :rules="nameRules"
+              label="Name"
+              required
+            ></v-text-field>
 
-    <v-text-field
-      v-model="email"
-      :rules="emailRules"
-      label="E-mail"
-      required
-    ></v-text-field>
+            <v-text-field
+              v-model="email"
+              :rules="emailRules"
+              label="E-mail"
+              required
+            ></v-text-field>
 
-    <v-select
-      v-model="select"
-      :items="items"
-      :rules="[v => !!v || 'Item is required']"
-      label="Item"
-      required
-    ></v-select>
+            <v-select
+              v-model="select"
+              :items="items"
+              :rules="[v => !!v || 'Item is required']"
+              label="Item"
+              required
+            ></v-select>
 
-    <v-checkbox
-      v-model="checkbox"
-      :rules="[v => !!v || 'You must agree to continue!']"
-      label="Do you agree?"
-      required
-    ></v-checkbox>
+            <v-checkbox
+              v-model="checkbox"
+              :rules="[v => !!v || 'You must agree to continue!']"
+              label="Do you agree?"
+              required
+            ></v-checkbox>
 
-    <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">
-      Validate
-    </v-btn>
+            <v-btn
+              :disabled="!valid"
+              color="success"
+              class="mr-4"
+              @click="validate"
+            >
+              Validate
+            </v-btn>
 
-    <v-btn color="error" class="mr-4" @click="reset">
-      Reset Form
-    </v-btn>
+            <v-btn color="error" class="mr-4" @click="reset">
+              Reset Form
+            </v-btn>
 
-    <v-btn color="warning" @click="resetValidation">
-      Reset Validation
-    </v-btn>
-  </v-form>
+            <v-btn color="warning" @click="resetValidation">
+              Reset Validation
+            </v-btn>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-responsive>
+  </v-container>
 </template>
 
 <script>
