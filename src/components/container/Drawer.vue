61aba4a57901b8ee3a46de272345707229685710
yoonlight <yoonlight12@gmail.com>
2021-05-10 17:57:46 +0900

    todo: impletment nav drawer sub group

--- src/components/container/Drawer.vue
+++ src/components/container/Drawer.vue
@@ -25,6 +25,53 @@
         </v-list-item-content>
       </v-list-item>
     </v-list>
+    <v-list>
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-title>Home</v-list-item-title>
+      </v-list-item>
+
+      <v-list-group :value="true" prepend-icon="mdi-account-circle">
+        <template v-slot:activator>
+          <v-list-item-title>Users</v-list-item-title>
+        </template>
+
+        <v-list-group :value="true" no-action sub-group>
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title>Admin</v-list-item-title>
+            </v-list-item-content>
+          </template>
+
+          <v-list-item v-for="([title, icon], i) in admins" :key="i" link>
+            <v-list-item-title v-text="title"></v-list-item-title>
+
+            <v-list-item-icon>
+              <v-icon v-text="icon"></v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+        </v-list-group>
+
+        <v-list-group no-action sub-group>
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title>Actions</v-list-item-title>
+            </v-list-item-content>
+          </template>
+
+          <v-list-item v-for="([title, icon], i) in cruds" :key="i" link>
+            <v-list-item-title v-text="title"></v-list-item-title>
+
+            <v-list-item-icon>
+              <v-icon v-text="icon"></v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+        </v-list-group>
+      </v-list-group>
+    </v-list>
   </v-navigation-drawer>
 </template>
 
@@ -35,7 +82,17 @@ export default {
   data: () => ({
     drawer: false,
     items: [],
-    right: null
+    right: null,
+    admins: [
+      ["Management", "mdi-account-multiple-outline"],
+      ["Settings", "mdi-cog-outline"]
+    ],
+    cruds: [
+      ["Create", "mdi-plus-outline"],
+      ["Read", "mdi-file-outline"],
+      ["Update", "mdi-update"],
+      ["Delete", "mdi-delete"]
+    ]
   }),
   async created() {
     this.items = await api.todo.theme().then(result => result.data);
